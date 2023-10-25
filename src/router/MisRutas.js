187a00f8acfdab5c54d605a378de48b56575e1c3
Maria Shaydrova <maria.sh.06@gmail.com>
2023-10-25 22:42:12 +0200

    rutas modificadas

--- src/router/MisRutas.js
+++ src/router/MisRutas.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import {HashRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import {Inicio} from '../components/Inicio';
 import {Portafolio} from '../components/Portafolio';
 import {Curriculum} from '../components/Curriculum';
