4f0e8f99a9c6ef4c7363db16104843f97c6887d2
Maria Shaydrova <maria.sh.06@gmail.com>
2023-10-25 18:22:25 +0200

    rutas modificadas

--- src/router/MisRutas.js
+++ src/router/MisRutas.js
@@ -19,8 +19,8 @@ export const MisRutas = () => {
         <section className='content' >
             <Routes>
                 <Route path='/' element={<Navigate to='/inicio' />}></Route>
-                <Route path='/inicio' element={<Inicio />}></Route>
-                <Route path='/portafolio' element={<Portafolio />}></Route>
+                <Route path='/portfolio' element={<Inicio />}></Route>
+                <Route path='/proyectos' element={<Portafolio />}></Route>
                 <Route path='/curriculum' element={<Curriculum />}></Route>
                 <Route path='/contacto' element={<Contacto />}></Route>
                 <Route path='/proyecto/:id' element={<Proyecto />}></Route>
