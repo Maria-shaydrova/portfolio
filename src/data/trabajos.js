27887f7d34c293369490ec41ca956b3c675dea3b
Maria Shaydrova <maria.sh.06@gmail.com>
2023-11-03 13:32:01 +0100

    proyecto cactus añadido

--- src/data/trabajos.js
+++ src/data/trabajos.js
@@ -10,16 +10,16 @@ export const trabajos = [
         'subtitulo': 'Elige una mascota y lanza diferentes ataques contra la mascota aleatoria del enemigo.',
         'descripcion': 'Se trata de un juego simple donde el primer paso es escoger una mascota. La mascota del enemigo se genera de forma aleatoria. Las dos mascotas lanzan los ataques disponibles y se realiza una comparacion. El juego termina cuando se han gastado todos los ataques o una de las dos mascotas ha perdido todas las vidas.'
     },
-    // {
-    //     'id': 'cactus',
-    //     'nombre': 'Web cactus',
-    //     'url': '...',
-    //     'tecnologias': 'HTML, CSS, JavaScript, PHP, MySQL',
-    //     'categorias': 'Desarollo web',
-    //     'subtitulo': '',
-    //     'descripcion': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui inventore eum ratione molestiae excepturi expedita, dolor incidunt quo reprehenderit consequuntur sequi, omnis non soluta, quidem delectus beatae. Numquam, cupiditate ducimus'
+    {
+        'id': 'cactus',
+        'nombre': 'Web cactus',
+        'url': 'github.com/Maria-shaydrova/web-cactus',
+        'tecnologias': 'HTML, CSS, JavaScript, PHP, MySQL',
+        'categorias': 'Desarollo web',
+        'subtitulo': 'Un ejemplo de una página web con diferentes funcionalidades. Instala el proyecto y pruébalo.',
+        'descripcion': 'Este proyecto incluye diferentes funcionalidades. Existe una sección de registro. Una vez registrado el usuario los datos se guardan en la base de datos y permite el acceso con las credenciales de usuario y contraseña. Las funcionalidades cambian en función del rol del usuario. El usuario con el rol admin tiene acceso a toda la base de datos, puede crear, modificar y eliminar usuarios, citas, noticias, etc.'
 
-    // },
+    },
     {
         'id': 'peliculas',
         'nombre': 'Web peliculas',
